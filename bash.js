920117830a7c735bb65a33ee9cf99c771ca29e9e
Josh Wintraub <joshbw4@gmail.com>
2019-09-23 12:35:34 -0400

    Complete process global exercises

--- bash.js
+++ bash.js
@@ -3,6 +3,10 @@ process.stdout.write('prompt >');
 process.stdin.on('data', (data) => {
   const cmd = data.toString().trim();
 
-  process.stdout.write('You typed: ' + cmd);
-  process.stdout.write('\nprompt > ')
-})
+  if (cmd === 'pwd') {
+  process.stdout.write(process.cwd()); // or __dirname
+  } else {
+    process.stdout.write('You typed: ' + cmd);
+  }
+  process.stdout.write('\nprompt > ');
+});

4624b315cc2627938cffa91e3d655e603a69f748
Josh Wintraub <joshbw4@gmail.com>
2019-09-23 13:02:27 -0400

    Added ls function capability to prompt line

--- bash.js
+++ bash.js
@@ -1,4 +1,5 @@
 const pwd = require('./pwd');
+const ls = require('./ls');
 
 process.stdout.write('prompt > ');
 
@@ -6,7 +7,9 @@ process.stdin.on('data', (data) => {
   const cmd = data.toString().trim();
 
   if (cmd === 'pwd') {
-  pwd();
+    pwd();
+  } else if (cmd === 'ls') {
+    ls();
   } else {
     process.stdout.write('You typed: ' + cmd);
     process.stdout.write('\nprompt > ');
--- pwd.js
+++ pwd.js
@@ -3,6 +3,4 @@ function pwd () {
   process.stdout.write('\nprompt > ');
 }
 
-console.log("we are inside pwd.js");
-
 module.exports = pwd;
